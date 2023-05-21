4bc6fc1ba66694bf7abc8558216e537a3db71a58
Odimayo David <odimayodavid7@gmail.com>
2023-05-21 15:20:32 +0100

    updated the end_index

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,6 +5,6 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ This function gets the range of index used to paginate """
-    start_index = page_size * (page - 1)
+    start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
