55d95017bfa7f6ee1f6cd6cad233692a73304d7d
Odimayo David <odimayodavid7@gmail.com>
2023-05-21 15:22:36 +0100

    updated the end_index

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Pagination helper function.
+"""This package contains the pagination helper function.
 """
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Retrieves the index range from a given page and page size.
+    """Retrieves the index range used for paginating.
     """
-    start = (page - 1) * page_size
-    end = start + page_size
-    return (start, end)
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
