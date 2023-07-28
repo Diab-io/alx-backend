8111a60e13b0aa34a03e6c3a7dc295e58c9a77fc
Odimayo David <odimayodavid7@gmail.com>
2023-07-28 15:16:21 +0100

    Modified the put method to discard data that is being stored with Fifo method when the cache storage is full

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" FIFOCache module """
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ class that implements fifo caching """
+
+    def put(self, key, item):
+        """ stores the data """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discard_key = list(self.cache_data.keys())[0]
+            del self.cache_data[discard_key]
+            print(f"DISCARD: {discard_key}")
+
+    def get(self, key):
+        """ retrieves the data """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
