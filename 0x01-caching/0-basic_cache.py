3dc08bfab556a20ff8c1ff70c35c7e79d5573554
Odimayo David <odimayodavid7@gmail.com>
2023-07-28 11:29:29 +0100

    removed whitespaces

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,12 +8,12 @@ class BasicCache(BaseCaching):
     """ A class with methods for storing data and retrieving """
     def put(self, key, item):
         """ stores the data """
-        if key == None or item == None:
+        if key is None or item is None:
             return
         self.cache_data[key] = item
-    
+
     def get(self, key):
         """ retrieves the data """
-        if key == None or key not in self.cache_data:
+        if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
